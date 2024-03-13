fa01c969705cd02812b7ab4eb81018321022dd62
supremood <lappay.john@gmail.com>
2024-03-14 07:56:37 +0800

    edit env

--- config/db.config.ts
+++ config/db.config.ts
@@ -13,6 +13,15 @@ const config: { [key: string]: Options } = {
     dialect: 'postgres',
     logging: false,
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.POSTGRES_URL,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+    dialect: 'postgres',
+    logging: false,
+  },
 };
 
 export default config;
